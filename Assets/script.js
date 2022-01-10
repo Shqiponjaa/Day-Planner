da8914852fb715d2769d93f8dcaf9b2af288b6ae
Shqiponjaa <94502514+Shqiponjaa@users.noreply.github.com>
2022-01-10 17:14:44 -0500

    commit

--- Assets/script.js
+++ Assets/script.js
@@ -1,6 +1,46 @@
 
     
-   
+    var input_textarea4 = document.querySelector("#toDo4");
+    var save_button4 = document.querySelector("#saveThis4");
+    input_textarea4.value = localStorage.getItem("12-PM");
+    save_button4.addEventListener("click", updateOutput4);
+  
+    function updateOutput4() {
+      localStorage.setItem("12-PM", input_textarea4.value);
+    }
+  
+    var input_textarea5 = document.querySelector("#toDo5");
+    var save_button5 = document.querySelector("#saveThis5");
+    input_textarea5.value = localStorage.getItem("1-PM");
+    save_button5.addEventListener("click", updateOutput5);
+  
+    function updateOutput5() {
+      localStorage.setItem("1-PM", input_textarea5.value);
+    }
+  
+    var input_textarea6 = document.querySelector("#toDo6");
+    var save_button6 = document.querySelector("#saveThis6");
+    input_textarea6.value = localStorage.getItem("2-PM");
+    save_button6.addEventListener("click", updateOutput6);
+  
+    function updateOutput6() {
+      localStorage.setItem("2-PM", input_textarea6.value);
+    }
+  
+    var input_textarea7 = document.querySelector("#toDo7");
+    var save_button7 = document.querySelector("#saveThis7");
+    input_textarea7.value = localStorage.getItem("3-PM");
+    save_button7.addEventListener("click", updateOutput7);
+  
+    function updateOutput7() {
+      localStorage.setItem("3-PM", input_textarea7.value);
+    }
+  
+    var input_textarea8 = document.querySelector("#toDo8");
+    var save_button8 = document.querySelector("#saveThis8");
+    input_textarea8.value = localStorage.getItem("4-PM");
+    save_button8.addEventListener("click", updateOutput8);
+  
     function updateOutput8() {
       localStorage.setItem("4-PM", input_textarea8.value);
     }
